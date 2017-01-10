3061ac19f2453f3e95a595177a9233ab061c7780
speaktoskm <speaktoskm@gmail.com>
2017-01-10 16:12:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,57 +29,59 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
+    if req.get("result").get("action") != "addTo5":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
-    result = urllib.urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
+    #baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    #yql_query = makeYqlQuery(req)
+#     if yql_query is None:
+#         return {}
+#     yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
+#     result = urllib.urlopen(yql_url).read()
+#     data = json.loads(result)
+    data = req.get("result").get("parameters").get("number")
+    res = makeWebhookResult(data) 
     return res
 
 
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
+# def makeYqlQuery(req):
+#     result = req.get("result")
+#     parameters = result.get("parameters")
+#     city = parameters.get("geo-city")
+#     if city is None:
+#         return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+#     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
 def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
+#     query = data.get('query')
+#     if query is None:
+#         return {}
 
-    result = query.get('results')
-    if result is None:
-        return {}
+#     result = query.get('results')
+#     if result is None:
+#         return {}
 
-    channel = result.get('channel')
-    if channel is None:
-        return {}
+#     channel = result.get('channel')
+#     if channel is None:
+#         return {}
 
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
+#     item = channel.get('item')
+#     location = channel.get('location')
+#     units = channel.get('units')
+#     if (location is None) or (item is None) or (units is None):
+#         return {}
 
-    condition = item.get('condition')
-    if condition is None:
-        return {}
+#     condition = item.get('condition')
+#     if condition is None:
+#         return {}
 
-    # print(json.dumps(item, indent=4))
+#     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
+#     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+#              ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    
+    speech = "You answer is " + str(data+5)
     print("Response:")
     print(speech)
 
